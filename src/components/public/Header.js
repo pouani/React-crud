07a0666b017eaa131a9bff21dcd5e7e4c3b7ea72
Ulrich POUANI <pouani@medicasure.com>
2023-01-06 16:04:24 +0100

    public route and component

---
+++ src/components/public/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/home">Accueil</Link></li>
+            <li><Link to="/service">Services</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
