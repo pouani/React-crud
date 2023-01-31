f2263e7badb42a3f274b4e4dfe2e820c1fb8cc0c
Ulrich POUANI <pouani@medicasure.com>
2023-01-31 15:59:27 +0100

    connexion avec l'api rest

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import './auth.css'
 
 const Login = () => {
-
+  let navigate = useNavigate();
   // const [login, setLogin] = useState("")
   // const [password, setPassword] = useState("")
 
   const [credentials, setCredentials] = useState({
-    login: '',
+    email: '',
     password: ''
   })
 
@@ -21,13 +23,19 @@ const Login = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     console.log(credentials)
+    axios.post('http://127.0.0.1:8002/api/login', credentials)
+        .then(res => {
+          console.log(res)
+          navigate('/admin')
+        })
+        .catch(err => console.log(err))
   }
 
   return (
     <form className='form-login' onSubmit={onSubmit}>
         <div className='group'>
-            <label htmlFor='login'>Identifiant</label><br />
-            <input type="text" name="login" value={credentials.login} onChange={onChange}/>
+            <label htmlFor='email'>Identifiant</label><br />
+            <input type="text" name="email" value={credentials.email} onChange={onChange}/>
         </div>
         <div className='group'>
             <label htmlFor='password'>Mot de passe</label><br />
