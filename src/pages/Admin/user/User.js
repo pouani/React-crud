8a91a2bc219d9289d1fd60c71f805cc29e23f1c3
Ulrich POUANI <pouani@medicasure.com>
2023-01-31 17:54:06 +0100

    affichage de la liste des users

--- src/pages/Admin/user/User.js
+++ src/pages/Admin/user/User.js
@@ -1,28 +1,51 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 import { userService } from '@/_services/user.service'
 
 const User = () => {
-  let navigate = useNavigate()
+  // let navigate = useNavigate()
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     userService.getAllUsers()
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data.Users)
+        setUsers(res.data.Users)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
-  const marcel = (user_id) => {
-    console.log("marcel")
-    navigate("../edit/"+user_id)
-  }
+  // const marcel = (user_id) => {
+  //   console.log("marcel")
+  //   navigate("../edit/"+user_id)
+  // }
   return (
     <div className='user'>
         User Liste
-        <button onClick={() => marcel(4)}>Click!</button>
+        {/* <button onClick={() => marcel(4)}>Click!</button> */}
+
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nom</th>
+                    <th>created at</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user, index) => (
+                    <tr key={index}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.created_at}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     </div>
   )
 }
